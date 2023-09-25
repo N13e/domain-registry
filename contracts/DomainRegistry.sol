2a6be4e6a33d613b60cb747429fdf8f381b166da
DubrovRV <n13e007@gmail.com>
2023-09-26 00:25:09 +0300

    feat: add contract and test

--- contracts/DomainRegistry.sol
+++ contracts/DomainRegistry.sol
@@ -1,9 +1,57 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
-
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+pragma solidity ^0.8.20;
 
 contract DomainRegistry {
+    uint256 public constant reservationCost = 100; // 0.01 ETH in wei
 
+    struct Domain {
+        address controller;
+        uint256 deposit;
+        bool isRegistered;
+    }
+
+    mapping(string => Domain) private domains;
+
+    event DomainRegistered(string domain, address indexed controller, uint256 deposit);
+    event DomainReleased(string domain, address indexed controller, uint256 deposit);
+
+    modifier isValidDomain(string memory domain) {
+        require(bytes(domain).length <= 6, "Domain length exceeds maximum of 6 characters");
+        require(bytes(domain)[0] != '.', "Domain cannot start with a period");
+        require(domains[domain].isRegistered == false, "Domain is already registered");
+        _;
+    }
+
+    function registerDomain(string memory domain) external payable isValidDomain(domain) {
+        require(msg.value >= reservationCost, "Insufficient deposit");
+
+        domains[domain] = Domain({
+            controller: msg.sender,
+            deposit: msg.value,
+            isRegistered: true
+        });
+
+        emit DomainRegistered(domain, msg.sender, msg.value);
+    }
+
+    function releaseDomain(string memory domain) external {
+        require(domains[domain].controller == msg.sender, "You are not the controller of this domain");
+
+        address controller = domains[domain].controller;
+        uint256 deposit = domains[domain].deposit;
+
+        domains[domain] = Domain({
+            controller: address(0),
+            deposit: 0,
+            isRegistered: false
+        });
+
+        payable(controller).transfer(deposit);
+
+        emit DomainReleased(domain, msg.sender, deposit);
+    }
+
+    function getDomainInfo(string memory domain) external view returns (address controller, uint256 deposit, bool isRegistered) {
+        return (domains[domain].controller, domains[domain].deposit, domains[domain].isRegistered);
+    }
 }
--- test/DomainRegistry.test.js
+++ test/DomainRegistry.test.js
@@ -1,7 +1,50 @@
+const { expect } = require('chai');
+
 describe('DomainRegistry', function () {
-  it("DomainRegistry", async () => {
-    const c = await ethers.deployContract("DomainRegistry");
-    const result = await c.target;
-    console.log(`\nresult: ${result}\n`);
+  let DomainRegistry;
+  let domainRegistry;
+  let owner;
+  let addr1;
+
+  beforeEach(async function () {
+    [owner, addr1] = await ethers.getSigners();
+
+    DomainRegistry = await ethers.getContractFactory('DomainRegistry');
+    domainRegistry = await DomainRegistry.deploy();
+  });
+
+  it('should register a domain', async function () {
+    const domain = 'com';
+    const reservationCost = 100; // 0.01 ETH in wei
+
+    // Register the domain
+    await expect(domainRegistry.connect(addr1).registerDomain(domain, { value: reservationCost }))
+        .to.emit(domainRegistry, 'DomainRegistered')
+        .withArgs(domain, addr1.address, reservationCost);
+
+    // Check domain info
+    const domainInfo = await domainRegistry.getDomainInfo(domain);
+    expect(domainInfo.controller).to.equal(addr1.address);
+    expect(domainInfo.deposit).to.equal(reservationCost);
+    expect(domainInfo.isRegistered).to.be.true;
+  });
+
+  it('should release a domain', async function () {
+    const domain = 'com';
+    const reservationCost = 100; // 0.01 ETH in wei
+
+    // Register the domain
+    await domainRegistry.connect(addr1).registerDomain(domain, { value: reservationCost });
+
+    // Release the domain
+    await expect(domainRegistry.connect(addr1).releaseDomain(domain))
+        .to.emit(domainRegistry, 'DomainReleased')
+        .withArgs(domain, addr1.address, reservationCost);
+
+    // Check domain info
+    const domainInfo = await domainRegistry.getDomainInfo(domain);
+    expect(domainInfo.controller).to.equal('0x0000000000000000000000000000000000000000');
+    expect(domainInfo.deposit).to.equal(0);
+    expect(domainInfo.isRegistered).to.be.false;
   });
 });
